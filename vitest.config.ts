730fe9d33a9a0502ea8ca167785dd8287c44f6dd
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2022-06-24 13:42:00 +0200

    Setup coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
     plugins: [tsconfigPaths(), react()],
+    test: {
+        coverage: {
+            reporter: ["html"],
+        },
+    },
 })
